e09d754962ae5f2fa42d192ea41fd0c093acddaa
Md. Billal Hossain Mozumder <mozumder0204@gmail.com>
2019-04-02 22:57:58 +0600

    laravel

---
+++ app/Http/Controllers/LandingPageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LandingPageController extends Controller
+{
+    public function index(){
+    	return view('landingPage.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('landingPage.index');
 });
+/****************** Landing Page ******************/
+Route::get('/', 'LandingPageController@index')->name('landingPage.index');
+//Route::post('/', 'LoginController@functionName');
+
+/****************** Login Page ******************/
+Route::get('/login', 'LoginController@index')->name('login.index');
+// Route::post('/login', 'LoginController@verify');
