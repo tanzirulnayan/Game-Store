084a23d1ba426a24126b5a59bfb7871875dd9559
Md. Billal Hossain Mozumder <mozumder0204@gmail.com>
2019-04-19 02:22:23 +0600

    signup index page

--- resources/views/signup/index.blade.php
+++ resources/views/signup/index.blade.php
@@ -1,54 +1,192 @@
 <!DOCTYPE html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Signup</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="{{ asset('bootstrap/css/bootstrap.min.css') }}" >
-    </head>
-    <body>
-{{--*******************Navbar Starts*******************--}}
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-  <div class="container">
-    <a class="navbar-brand" href="{{route('landingPage.index')}}"> Game Store</a>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-            <li>
-            <a class="nav-link" href="{{route('login.index')}}">Login
-                    <span class="sr-only">(current)</span>
-                </a>
-            </li>
-            <li class="nav-item active">
-            <a class="nav-link" href="{{route('signup.index')}}">Signup</a>
-            </li>
-        </ul>
-    </div>
-  </div>
-</nav>
-{{--*******************Navbar Ends*******************--}}
+<html lang="en">
 
-<div class="container">
-    <div class="row">
-        <form class="col-md-4 offset-md-4">
-            <br><br><br><br><br>
-            <div >
-                <a href="{{route('signup.gamer')}}">Sign Up as Gamer</a>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{asset('custom')}}/assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="{{asset('custom')}}/assets/img/favicon.png">
+  <title>
+    Login
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <!-- Nucleo Icons -->
+  <link href="{{asset('custom')}}/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link href="{{asset('custom')}}/assets/css/blk-design-system.css?v=1.0.0" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="{{asset('custom')}}/assets/demo/demo.css" rel="stylesheet" />
+</head>
+
+<body class="index-page">
+  {{-- ------------------------------------- Navbar ------------------------------------- --}}
+  <nav class="navbar navbar-expand-lg fixed-top navbar-transparent " color-on-scroll="100">
+      <div class="container">
+        <div class="navbar-translate">
+          <a class="navbar-brand" href="{{route('landingPage.index')}}" rel="tooltip" title="Explore the Gaming World" data-placement="bottom" target="_blank">
+            <span>Game•</span> Store
+          </a>
+        </div>
+        <div class="collapse navbar-collapse justify-content-end" id="navigation">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('landingPage.index')}}">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{route('login.index')}}">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{route('signup.index')}}">Signup</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    {{-- -- ------------------------------------- End Navbar --------------------------------------- --}}
+  <div class="wrapper">
+
+    <div class="main">
+      <div class="section section-signup">
+        <div class="container">
+          <div class="squares square-1"></div>
+          <div class="squares square-2"></div>
+          <div class="squares square-3"></div>
+          <div class="squares square-4"></div>
+          <div class="row row-grid justify-content-between align-items-center">
+            <div class="col-lg-6 mb-lg-auto">
+              <div class="card card-register">
+                <div class="card-header">
+                  <img class="card-img" src="assets/img/square1.png" alt="Card image">
+                  <h4 class="card-title">Signup</h4>
+                </div>
+              </div>
             </div>
-            <br><br>
-            <div >
-                <a href="{{route('signup.developer')}}">Sign Up as Game Developer</a>
-            </div>
-            <br><br>
-            <div >
-                <a href="#">Sign Up as Moderator</a>
-            </div>
-            
-        </form> 
+            <nav class="navbar navbar-expand-lg bg-success">
+                <div class="container">   
+                    <a class="navbar-brand" href="{{route('signup.gamer')}}">Signup as GAMER</a> 
+                </div>
+            </nav>
+            <nav class="navbar navbar-expand-lg bg-info">
+                <div class="container">
+                    <a class="navbar-brand" href="{{route('signup.developer')}}">Signup as DEVELOPER</a>    
+                </div>
+            </nav>
+            <nav class="navbar navbar-expand-lg bg-primary">
+                <div class="container">
+                    <a class="navbar-brand" href="#">Signup as MODERATOR</a>    
+                </div>
+            </nav>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-        
-        
-        <script type="text/javascript" src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
-    </body>
+    {{-- <footer class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <h1 class="title">BLK•</h1>
+          </div>
+          <div class="col-md-3">
+            <ul class="nav">
+              <li class="nav-item">
+                <a href="{{asset('custom')}}/index.html" class="nav-link">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{asset('custom')}}/examples/landing-page.html" class="nav-link">
+                  Landing
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{asset('custom')}}/examples/register-page.html" class="nav-link">
+                  Register
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{asset('custom')}}/examples/profile-page.html" class="nav-link">
+                  Profile
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <ul class="nav">
+              <li class="nav-item">
+                <a href="https://creative-tim.com/contact-us" class="nav-link">
+                  Contact Us
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="https://creative-tim.com/about-us" class="nav-link">
+                  About Us
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="https://creative-tim.com/blog" class="nav-link">
+                  Blog
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="https://opensource.org/licenses/MIT" class="nav-link">
+                  License
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <h3 class="title">Follow us:</h3>
+            <div class="btn-wrapper profile">
+              <a target="_blank" href="https://twitter.com/creativetim" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a target="_blank" href="https://www.facebook.com/creativetim" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Like us">
+                <i class="fab fa-facebook-square"></i>
+              </a>
+              <a target="_blank" href="https://dribbble.com/creativetim" class="btn btn-icon btn-neutral  btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
+                <i class="fab fa-dribbble"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer> --}}
+  </div>
+  <!--   Core JS Files   -->
+  <script src="{{asset('custom')}}/assets/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="{{asset('custom')}}/assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="{{asset('custom')}}/assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+  <script src="{{asset('custom')}}/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+  <script src="{{asset('custom')}}/assets/js/plugins/bootstrap-switch.js"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="{{asset('custom')}}/assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <!-- Chart JS -->
+  <script src="{{asset('custom')}}/assets/js/plugins/chartjs.min.js"></script>
+  <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+  <script src="{{asset('custom')}}/assets/js/plugins/moment.min.js"></script>
+  <script src="{{asset('custom')}}/assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+  <!-- Black Dashboard DEMO methods, don't include it in your project! -->
+  <script src="{{asset('custom')}}/assets/demo/demo.js"></script>
+  <!-- Control Center for Black UI Kit: parallax effects, scripts for the example pages etc -->
+  <script src="{{asset('custom')}}/assets/js/blk-design-system.min.js?v=1.0.0" type="text/javascript"></script>
+  <script>
+    $(document).ready(function() {
+      blackKit.initDatePicker();
+      blackKit.initSliders();
+    });
+
+    function scrollToDownload() {
+
+      if ($('.section-download').length != 0) {
+        $("html, body").animate({
+          scrollTop: $('.section-download').offset().top
+        }, 1000);
+      }
+    }
+  </script>
+</body>
+
 </html>
