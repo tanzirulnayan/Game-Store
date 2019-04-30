7df24f80f13880e30526b6f3c621b598c9495074
Tanzirul Haque <tanzirulhaque7@gmail.com>
2019-04-30 09:02:09 +0600

    gamer statistics done

--- resources/views/layouts/gamer/app.blade.php
+++ resources/views/layouts/gamer/app.blade.php
@@ -30,6 +30,9 @@
           <li class="nav-item">
               <a class="nav-link" href="{{route('gamer.index')}}">Home</a>
           </li>
+          <li class="nav-item">
+              <a class="nav-link" href="{{route('gamer.Statistics')}}">Statistics</a>
+          </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <p>
