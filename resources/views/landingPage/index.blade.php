0cf52f10043176674b3d4e676ebae88fc4f4426e
Tanzirul Haque <tanzirulhaque7@gmail.com>
2019-07-06 14:27:58 +0600

    landingPage -> index view formatted

--- resources/views/landingPage/index.blade.php
+++ resources/views/landingPage/index.blade.php
@@ -28,7 +28,7 @@
       <div class="collapse navbar-collapse justify-content-end" id="navigation">
         <ul class="navbar-nav">
           <li class="nav-item">
-              <a class="nav-link" href="{{route('landingPage.index')}}">Home</a>
+            <a class="nav-link" href="{{route('landingPage.index')}}">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="{{route('login.index')}}">Login</a>
@@ -48,45 +48,45 @@
         <div class="container">
           <div class="row">
             <div class="col-lg-5 col-md-6 offset-lg-0 offset-md-3">
-                <div id="square7" class="square square-7"></div>
-                <h1 style="color:white; fontisize:400%;">E  X  P  L  O  R  E</h1>
-                <h1 style="color:white; fontisize:400%;">THE WORLD OF GAMES</h1>
-            <div class="register-bg"></div>
-            <div id="square1" class="square square-1"></div>
-            <div id="square2" class="square square-2"></div>
-            <div id="square3" class="square square-3"></div>
-            <div id="square4" class="square square-4"></div>
-            <div id="square5" class="square square-5"></div>
-            <div id="square6" class="square square-6"></div>
-        </div>
-      </div>
-    </div>
-    <footer class="footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-3">
-            <h1 class="title">Game•Store</h1>
+              <div id="square7" class="square square-7"></div>
+              <h1 style="color:white; fontisize:400%;">E X P L O R E</h1>
+              <h1 style="color:white; fontisize:400%;">THE WORLD OF GAMES</h1>
+              <div class="register-bg"></div>
+              <div id="square1" class="square square-1"></div>
+              <div id="square2" class="square square-2"></div>
+              <div id="square3" class="square square-3"></div>
+              <div id="square4" class="square square-4"></div>
+              <div id="square5" class="square square-5"></div>
+              <div id="square6" class="square square-6"></div>
+            </div>
           </div>
         </div>
+        <footer class="footer">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-3">
+                <h1 class="title">Game•Store</h1>
+              </div>
+            </div>
+          </div>
+        </footer>
       </div>
-    </footer>
-  </div>
-  <!--   Core JS Files   -->
-  <script src="{{asset('custom')}}/assets/js/core/jquery.min.js" type="text/javascript"></script>
-  <script src="{{asset('custom')}}/assets/js/core/popper.min.js" type="text/javascript"></script>
-  <script src="{{asset('custom')}}/assets/js/core/bootstrap.min.js" type="text/javascript"></script>
-  <script src="{{asset('custom')}}/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-  <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
-  <script src="{{asset('custom')}}/assets/js/plugins/bootstrap-switch.js"></script>
-  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-  <script src="{{asset('custom')}}/assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
-  <!-- Chart JS -->
-  <script src="{{asset('custom')}}/assets/js/plugins/chartjs.min.js"></script>
-  <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
-  <script src="{{asset('custom')}}/assets/js/plugins/moment.min.js"></script>
-  <script src="{{asset('custom')}}/assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
-  <!-- Control Center for Black UI Kit: parallax effects, scripts for the example pages etc -->
-  <script src="{{asset('custom')}}/assets/js/blk-design-system.min.js?v=1.0.0" type="text/javascript"></script>
+      <!--   Core JS Files   -->
+      <script src="{{asset('custom')}}/assets/js/core/jquery.min.js" type="text/javascript"></script>
+      <script src="{{asset('custom')}}/assets/js/core/popper.min.js" type="text/javascript"></script>
+      <script src="{{asset('custom')}}/assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+      <script src="{{asset('custom')}}/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+      <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+      <script src="{{asset('custom')}}/assets/js/plugins/bootstrap-switch.js"></script>
+      <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+      <script src="{{asset('custom')}}/assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+      <!-- Chart JS -->
+      <script src="{{asset('custom')}}/assets/js/plugins/chartjs.min.js"></script>
+      <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+      <script src="{{asset('custom')}}/assets/js/plugins/moment.min.js"></script>
+      <script src="{{asset('custom')}}/assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+      <!-- Control Center for Black UI Kit: parallax effects, scripts for the example pages etc -->
+      <script src="{{asset('custom')}}/assets/js/blk-design-system.min.js?v=1.0.0" type="text/javascript"></script>
 </body>
 
 </html>
