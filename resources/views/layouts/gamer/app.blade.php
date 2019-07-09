426804d33827a244b37d505875d205f7fe5d8de8
Tanzirul Haque <tanzirul.haque@outlook.com>
2019-07-09 10:46:34 +0600

    gamer layout view formatted

--- resources/views/layouts/gamer/app.blade.php
+++ resources/views/layouts/gamer/app.blade.php
@@ -28,10 +28,10 @@
       <div class="collapse navbar-collapse justify-content-end" id="navigation">
         <ul class="navbar-nav">
           <li class="nav-item">
-              <a class="nav-link" href="{{route('gamer.index')}}">Home</a>
+            <a class="nav-link" href="{{route('gamer.index')}}">Home</a>
           </li>
           <li class="nav-item">
-              <a class="nav-link" href="{{route('gamer.Statistics')}}">Statistics</a>
+            <a class="nav-link" href="{{route('gamer.Statistics')}}">Statistics</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
