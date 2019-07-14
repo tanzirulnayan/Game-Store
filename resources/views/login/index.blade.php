9e2001786626a710c6e162650b905a6a5d74737b
Tanzirul Haque <tanzirul.haque@outlook.com>
2019-07-14 17:37:50 +0600

    login format WIP

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -6,9 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="{{asset('custom')}}/assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="{{asset('custom')}}/assets/img/favicon.png">
-  <title>
-    Login
-  </title>
+  <title>Login</title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
   <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
@@ -23,28 +21,28 @@
 <body class="index-page">
   {{-- ------------------------------------- Navbar ------------------------------------- --}}
   <nav class="navbar navbar-expand-lg fixed-top navbar-transparent " color-on-scroll="100">
-      <div class="container">
-        <div class="navbar-translate">
-          <a class="navbar-brand" href="{{route('landingPage.index')}}">
-            <span>Game•</span> Store
-          </a>
-        </div>
-        <div class="collapse navbar-collapse justify-content-end" id="navigation">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('landingPage.index')}}">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{route('login.index')}}">Login</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{route('signup.index')}}">Signup</a>
-            </li>
-          </ul>
-        </div>
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="{{route('landingPage.index')}}">
+          <span>Game•</span> Store
+        </a>
       </div>
-    </nav>
-    {{-- -- ------------------------------------- End Navbar --------------------------------------- --}}
+      <div class="collapse navbar-collapse justify-content-end" id="navigation">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('landingPage.index')}}">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('login.index')}}">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('signup.index')}}">Signup</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  {{-- -- ------------------------------------- End Navbar --------------------------------------- --}}
   <div class="wrapper">
     <div class="main">
       <div class="section section-signup">
@@ -67,56 +65,54 @@
 
                     @if ($errors->has('USERNAME'))
                     <div class="input-group has-danger">
-                        <div class="input-group-prepend">
-                          <div class="input-group-text">
-                            <i class="tim-icons icon-single-02"></i>
-                          </div>
+                      <div class="input-group-prepend">
+                        <div class="input-group-text">
+                          <i class="tim-icons icon-single-02"></i>
                         </div>
-                        <input type="text" class="form-control" name="USERNAME" 
-                        placeholder="{{$errors->getBag('default')->first('USERNAME')}}" >
-                        
                       </div>
+                      <input type="text" class="form-control" name="USERNAME" placeholder="{{$errors->getBag('default')->first('USERNAME')}}">
+
+                    </div>
                     @else
                     <div class="input-group">
-                        <div class="input-group-prepend">
-                          <div class="input-group-text">
-                            <i class="tim-icons icon-single-02"></i>
-                          </div>
+                      <div class="input-group-prepend">
+                        <div class="input-group-text">
+                          <i class="tim-icons icon-single-02"></i>
                         </div>
-                        <input type="text" class="form-control" name="USERNAME" placeholder="Username..." >
                       </div>
-                  @endif
-                  @if ($errors->has('PASSWORD'))
-                  <div class="input-group has-danger">
+                      <input type="text" class="form-control" name="USERNAME" placeholder="Username...">
+                    </div>
+                    @endif
+                    @if ($errors->has('PASSWORD'))
+                    <div class="input-group has-danger">
                       <div class="input-group-prepend">
                         <div class="input-group-text">
                           <i class="tim-icons icon-lock-circle"></i>
                         </div>
                       </div>
-                      <input type="password" class="form-control" name="PASSWORD" 
-                      placeholder=" {{$errors->getBag('default')->first('PASSWORD')}}" >
+                      <input type="password" class="form-control" name="PASSWORD" placeholder=" {{$errors->getBag('default')->first('PASSWORD')}}">
                     </div>
-                  @else
-                  <div class="input-group">
+                    @else
+                    <div class="input-group">
                       <div class="input-group-prepend">
                         <div class="input-group-text">
                           <i class="tim-icons icon-lock-circle"></i>
                         </div>
                       </div>
-                      <input type="password" class="form-control" name="PASSWORD" placeholder="Password..." >
+                      <input type="password" class="form-control" name="PASSWORD" placeholder="Password...">
                     </div>
-                @endif
+                    @endif
 
                     <button class="btn btn-primary btn-round" type="submit">
-                        <i class="tim-icons icon-controller"></i> Login
+                      <i class="tim-icons icon-controller"></i> Login
                     </button>
                   </form>
                   <br>
                   <label class="form-check-label">
-                      <span class="form-check-sign"></span>
-                      <p class="text-success">Need an account? </p>
-                      <a href="{{route('signup.index')}}">Signup</a>
-                   </label>
+                    <span class="form-check-sign"></span>
+                    <p class="text-success">Need an account? </p>
+                    <a href="{{route('signup.index')}}">Signup</a>
+                  </label>
                 </div>
               </div>
             </div>
@@ -133,68 +129,66 @@
           <div class="col-md-3">
             <ul class="nav">
               <li class="nav-item">
-                <a href="{{asset('custom')}}/index.html" class="nav-link">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{asset('custom')}}/examples/landing-page.html" class="nav-link">
-                  Landing
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{asset('custom')}}/examples/register-page.html" class="nav-link">
-                  Register
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{asset('custom')}}/examples/profile-page.html" class="nav-link">
-                  Profile
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-md-3">
-            <ul class="nav">
-              <li class="nav-item">
-                <a href="https://creative-tim.com/contact-us" class="nav-link">
-                  Contact Us
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="https://creative-tim.com/about-us" class="nav-link">
-                  About Us
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="https://creative-tim.com/blog" class="nav-link">
-                  Blog
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="https://opensource.org/licenses/MIT" class="nav-link">
-                  License
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-md-3">
-            <h3 class="title">Follow us:</h3>
-            <div class="btn-wrapper profile">
-              <a target="_blank" href="https://twitter.com/creativetim" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
-                <i class="fab fa-twitter"></i>
-              </a>
-              <a target="_blank" href="https://www.facebook.com/creativetim" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Like us">
-                <i class="fab fa-facebook-square"></i>
-              </a>
-              <a target="_blank" href="https://dribbble.com/creativetim" class="btn btn-icon btn-neutral  btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
-                <i class="fab fa-dribbble"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer> --}}
+                <a href="{{asset('custom')}}/index.html" class="nav-link">Home</a>
+    </li>
+    <li class="nav-item">
+      <a href="{{asset('custom')}}/examples/landing-page.html" class="nav-link">
+        Landing
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="{{asset('custom')}}/examples/register-page.html" class="nav-link">
+        Register
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="{{asset('custom')}}/examples/profile-page.html" class="nav-link">
+        Profile
+      </a>
+    </li>
+    </ul>
+  </div>
+  <div class="col-md-3">
+    <ul class="nav">
+      <li class="nav-item">
+        <a href="https://creative-tim.com/contact-us" class="nav-link">
+          Contact Us
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="https://creative-tim.com/about-us" class="nav-link">
+          About Us
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="https://creative-tim.com/blog" class="nav-link">
+          Blog
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="https://opensource.org/licenses/MIT" class="nav-link">
+          License
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div class="col-md-3">
+    <h3 class="title">Follow us:</h3>
+    <div class="btn-wrapper profile">
+      <a target="_blank" href="https://twitter.com/creativetim" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a target="_blank" href="https://www.facebook.com/creativetim" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Like us">
+        <i class="fab fa-facebook-square"></i>
+      </a>
+      <a target="_blank" href="https://dribbble.com/creativetim" class="btn btn-icon btn-neutral  btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
+        <i class="fab fa-dribbble"></i>
+      </a>
+    </div>
+  </div>
+  </div>
+  </div>
+  </footer> --}}
   </div>
   <!--   Core JS Files   -->
   <script src="{{asset('custom')}}/assets/js/core/jquery.min.js" type="text/javascript"></script>
