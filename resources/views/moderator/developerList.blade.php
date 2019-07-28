48a236d7ab036879921e080db713ff4a858bd537
Tanzirul Haque <tanzirul.haque@outlook.com>
2019-07-28 15:19:40 +0600

    Moderator -> developerList view formatted

--- resources/views/moderator/developerList.blade.php
+++ resources/views/moderator/developerList.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.moderator.app')
-
 @section('content')
+
 <body class="profile-page">
-  
   <div class="wrapper">
     <section class="section">
       <div class="container">
@@ -17,54 +16,39 @@
           </div>
         </div>
         <div class="tab-content tab-subcategories">
-            <div class="tab-pane active" id="linka">
-         <div class="table-responsive">
-            <table class="table tablesorter " id="plain-table">
-              <thead class=" text-primary">
-                <tr>
-                  <th class="header">
-                    USERNAME
-                  </th>
-                  <th class="header">
-                    Developer Name
-                  </th>
-                
-                  <th class="header">
-                      ACTION
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-              @foreach($list as $list)
-                <tr>
-                  <td>
-                    {{$list->USERNAME}}
-                  </td>
-                  <td>
-                    {{$list->DEV_NAME}}
-                  </td>
-             
-                  <td>
-                    <a class="nav-link" href="{{route('moderator.viewProfileDeveloper', $list->USERNAME)}}">View Profile</a> 
-                    <a class="nav-link" href="{{route('moderator.deleteDeveloperToDB', $list->USERNAME)}}">Remove Account</a> 
-                    <a class="nav-link" href="{{route('moderator.allGames', $list->USERNAME)}}">Game List</a> 
-
-                         
-                  </td>
-                </tr>
-               @endforeach
-              </tbody>
-            </table>
+          <div class="tab-pane active" id="linka">
+            <div class="table-responsive">
+              <table class="table tablesorter " id="plain-table">
+                <thead class=" text-primary">
+                  <tr>
+                    <th class="header">Username</th>
+                    <th class="header">Developer Name</th>
+                    <th class="header">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach($list as $list)
+                  <tr>
+                    <td>{{$list->USERNAME}}</td>
+                    <td>{{$list->DEV_NAME}}</td>
+                    <td>
+                      <a class="nav-link" href="{{route('moderator.viewProfileDeveloper', $list->USERNAME)}}">View Profile</a>
+                      <a class="nav-link" href="{{route('moderator.deleteDeveloperToDB', $list->USERNAME)}}">Remove Account</a>
+                      <a class="nav-link" href="{{route('moderator.allGames', $list->USERNAME)}}">Game List</a>
+                    </td>
+                  </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-      </div>
     </section>
-   </div>
+  </div>
 
-   @endsection
+  @endsection
 
-
-@section('title')
-Gamer List | Moderator
-@endsection
+  @section('title')
+  Gamer List | Moderator
+  @endsection
