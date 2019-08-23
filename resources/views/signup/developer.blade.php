db8016ab2a721c8152797fde66a486c07bc6a4e0
Tanzirul Haque <tanzirulhaque7@gmail.com>
2019-08-23 22:19:29 +0600

    Signup -> developer WIP

--- resources/views/signup/developer.blade.php
+++ resources/views/signup/developer.blade.php
@@ -6,9 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="{{asset('custom')}}/assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="{{asset('custom')}}/assets/img/favicon.png">
-  <title>
-    Signup | Developer
-  </title>
+  <title>Signup | Developer</title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
   <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
@@ -296,71 +294,9 @@
           <div class="col-md-3">
             <h1 class="title">Game•Store</h1>
           </div>
-          {{-- <div class="col-md-3">
-            <ul class="nav">
-              <li class="nav-item">
-                <a href="{{asset('custom')}}/index.html" class="nav-link">
-          Home
-          </a>
-          </li>
-          <li class="nav-item">
-            <a href="{{asset('custom')}}/examples/landing-page.html" class="nav-link">
-              Landing
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="{{asset('custom')}}/examples/register-page.html" class="nav-link">
-              Register
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="{{asset('custom')}}/examples/profile-page.html" class="nav-link">
-              Profile
-            </a>
-          </li>
-          </ul>
         </div>
-        <div class="col-md-3">
-          <ul class="nav">
-            <li class="nav-item">
-              <a href="https://creative-tim.com/contact-us" class="nav-link">
-                Contact Us
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="https://creative-tim.com/about-us" class="nav-link">
-                About Us
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="https://creative-tim.com/blog" class="nav-link">
-                Blog
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="https://opensource.org/licenses/MIT" class="nav-link">
-                License
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-3">
-          <h3 class="title">Follow us:</h3>
-          <div class="btn-wrapper profile">
-            <a target="_blank" href="https://twitter.com/creativetim" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
-              <i class="fab fa-twitter"></i>
-            </a>
-            <a target="_blank" href="https://www.facebook.com/creativetim" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Like us">
-              <i class="fab fa-facebook-square"></i>
-            </a>
-            <a target="_blank" href="https://dribbble.com/creativetim" class="btn btn-icon btn-neutral  btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
-              <i class="fab fa-dribbble"></i>
-            </a>
-          </div>
-        </div> --}}
       </div>
-  </div>
-  </footer>
+    </footer>
   </div>
   <!--   Core JS Files   -->
   <script src="{{asset('custom')}}/assets/js/core/jquery.min.js" type="text/javascript"></script>
@@ -379,4 +315,5 @@
   <!-- Control Center for Black UI Kit: parallax effects, scripts for the example pages etc -->
   <script src="{{asset('custom')}}/assets/js/blk-design-system.min.js?v=1.0.0" type="text/javascript"></script>
 </body>
+
 </html>
